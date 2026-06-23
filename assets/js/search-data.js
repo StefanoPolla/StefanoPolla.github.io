31993d66690d9d0689ff0287ea74f563eb629c5d
StefanoPolla <44360102+StefanoPolla@users.noreply.github.com>
2026-06-23 13:01:19 +0000

    Deploying to gh-pages from @ StefanoPolla/StefanoPolla.github.io@b36fa172da0916be067a9948632442ee1474a317 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
+        },{id: "nav-openings",
+          title: "openings",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/openings/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
